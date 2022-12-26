39cb7393d6453cffca1a636fe916975856f8f252
Luis E. Sanchez Jr <luchopro@Luiss-MacBook-Pro.local>
2022-12-25 23:20:04 -0500

    Added pastes-data.js to create a server with saved data in memory and explore the uses of static data, this particular API was used to store and access code snippets/plain text.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const app = express();
-
 // TODO: Follow instructions in the checkpoint to implement ths API.
+const pastes = require("./data/pastes-data");
+
+//using route params to return one paste object by id and turning it into json
+app.use("/pastes/:pasteId", (req, res, next) => {
+  const { pasteId } = req.params;
+  const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
+
+  if (foundPaste) {
+    res.json({ data: foundPaste });
+  } else {
+    next(`Paste id not found: ${pasteId}`);
+  }
+});
+app.use("/pastes", (req, res) => {
+  res.json({ data: pastes });//taking the pastes data and responding back to client with JSON
+});
 
 // Not found handler
 app.use((request, response, next) => {
